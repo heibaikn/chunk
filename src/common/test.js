637cc0d46b9b1aeb0eb270d3b9702df0f7fdb1b4
kuangning <kuangning@rayvision.com>
2018-01-18 09:54:26 +0800

    chunk  pre app js

---
+++ src/common/test.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+const CONST_HEADER = () => ({
+  userKey: localStorage.getItem('userKey') || '',
+  version: '1.0.0',
+  signature: 'rayvision2017',
+  platform: localStorage.getItem('platform') || 2,
+  channel: 2,
+  languageFlag: localStorage.getItem('languageFlag') || 0,
+});
+
+var instance = axios.create({
+
+});
+instance
+  .interceptors
+  .request
+  .use(config => {
+    Object.assign(config.headers, CONST_HEADER());
+    return config
+  });
+instance.interceptors.response.use((response) => {
+  return response.data;
+});
+
+instance.post('/api/rendering/user/queryUser').then(v => {
+  console.log('pre',v);
+})
