f28783d4445368d2ba1f7ef27c0daeaaf6c37c63
kuangning <kuangning@rayvision.com>
2018-01-18 10:09:18 +0800

    remove axios function to  native ajax  xhrhttprequest

--- src/common/test.js
+++ src/common/test.js
@@ -1,27 +1,39 @@
-import axios from 'axios';
-const CONST_HEADER = () => ({
-  userKey: localStorage.getItem('userKey') || '',
-  version: '1.0.0',
-  signature: 'rayvision2017',
-  platform: localStorage.getItem('platform') || 2,
-  channel: 2,
-  languageFlag: localStorage.getItem('languageFlag') || 0,
-});
+// 定义创建XMLHttpRequest对象的函数
+function getXhr() {
+  // 声明XMLHttpRequest对象
+  var xhr;
+  // 根据不同浏览器创建
+  if (window.XMLHttpRequest) {
+    // 其他浏览器
+    xhr = new XMLHttpRequest();
+  } else {
+    // IE浏览器(8及之前)
+    xhr = new ActiveXObject("Microsoft.XMLHttp");
+  }
+  // 返回XMLHttpRequest对象
+  return xhr;
+}
 
-var instance = axios.create({
+var xhr = getXhr();
+xhr.open("get", "/api/rendering/user/loadLanguagePack", true);		//open() 提交数据 
+xhr.send();
+xhr.onreadystatechange = function () {
+  // 保证服务器端响应的数据发送完毕
+  if (xhr.readyState == 4) {
+    // 保证这次请求必须是成功的
+    if (xhr.status == 200) {
+      // 接收服务器端的数据
+      var json = xhr.responseText;//获取数据 json格式的字符串
+      console.log(json);
+      /*
+       * 使用eval()函数进行转换
+       * * 使用"()"将其包裹,eval()函数强制将其转换为JSON格式(javascript代码)
+       * * 不使用"()"将其包裹,eval()函数将其识别为一个空的代码块
+       */
+      var data = eval("(" + json + ")");//
+      console.log(data);
+    }
+  }
+}
 
-});
-instance
-  .interceptors
-  .request
-  .use(config => {
-    Object.assign(config.headers, CONST_HEADER());
-    return config
-  });
-instance.interceptors.response.use((response) => {
-  return response.data;
-});
-
-instance.post('/api/rendering/user/queryUser').then(v => {
-  console.log('pre',v);
-})
+console.log(123);
